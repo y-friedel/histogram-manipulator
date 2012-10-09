bb47ef5685d179b71e3d71ee8e0405b266ac6067
friedel.yoann@gmail.com <friedel.yoann@gmail.com@7962e06d-3c59-e173-c720-0b4dc214c8f0>
2012-10-09 19:24:59 +0000

    version fonctionnelle chez ouam

--- MMTI_GUI/main.cpp
+++ MMTI_GUI/main.cpp
@@ -1,28 +1,83 @@
-#include <QtGui/QApplication>
-#include "qmlapplicationviewer.h"
+#include "Image.hpp"
+#include "Histogramme.hpp"
+#include "Fonction.hpp"
+#include "Traitement.hpp"
+#include <iostream>
 
-#include "MainWindow.h"
-
-Q_DECL_EXPORT int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
-/*    QScopedPointer<QApplication> app(createApplication(argc, argv));
+	if(argc != 3)
+	{
+		std::cout << "wrong parameter" << std::endl;
+		return -1;	
+	}
+		std::string input = std::string(argv[1]);
+		std::string output = std::string(argv[2]);
 
-    QmlApplicationViewer viewer;
-    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-    viewer.setMainQmlFile(QLatin1String("qml/MMTI_GUI/main.qml"));
-    viewer.showExpanded();
+	Image im = Image();
+	std::string exp = "./data/Hdepart.txt";
 
-    QString nomImage("src_name");
-    //viewer.rootContext()->setContextProperty("src_name", nomImage);
+	im.load(input.c_str());
+
+	Image imcpy = Image(im);
+	Histogramme histo = Histogramme(im);
+	histo.exporter_TXT(exp.c_str());
+
+//	imcpy->saveBin(output.c_str());
+	imcpy.saveAscii(output.c_str());
+	histo.save("Histogramme_esper");	
+
+	std::vector<int> temp = std::vector<int>();
+
+	//histogramme plat
+	for(int i=0; i<256;i++)	temp.push_back(1);
+	  
+	Histogramme newHisto = Histogramme(temp);    
+
+	//newHisto.setNombrePixels(50000);	 
+	newHisto.save("Histogramme2");
+    
+	Fonction fonction = Fonction(255);
+	
+	
+	Image image = Image("./data/rs2.pgm");
+	Image image1 = Image(image);
+	
+	/*fonction.specification(im, image1, newHisto);
+	image1.saveAscii("./data/specification.pgm");
+
+	fonction.negatif(im, image1);
+	im.saveAscii("./data/t1a.pgm");
+	image1.saveAscii("./data/t1b.pgm");
+		fonction.negatif(im, image1);
+	im.saveAscii("./data/t2a.pgm");
+	image1.saveAscii("./data/t2b.pgm");
 
 
-    return app->exec();*/
+	Histogramme histo1 = Histogramme(image1);
+	histo1.exporter_TXT("./data/Hspecification.txt");
+	
+	fonction.egalisation(im, image1);
+	image1.saveAscii("./data/egalisation.pgm");
+	
+	Histogramme histo2 = Histogramme(image1);
+	histo2.exporter_TXT("./data/Hegalisation.txt");	
+	
+	Image image2;
+	fonction.recadrage(im, image2);
+	image2.saveAscii("./data/recadrage.pgm");
+	
+	Histogramme histo3 = Histogramme(image2);
+	histo3.exporter_TXT("./data/Hrecadrage.txt");*/
+	
+	Traitement traitement = Traitement();
+	//traitement.versionGlissante(image, image1, newHisto, 1);
+traitement.specificationDansFenetre(image, image1, newHisto, 2, 5, 0, 1);
 
-    QApplication app(argc, argv);
-    MainWindow m;
-    m.show();
-    return app.exec();
+std::cout<<"ET LA ?"<<std::endl;
+	//image1.saveAscii("./data/versionGlissante.pgm");
+	
 
-
-
+	
+	return 0;
 }
